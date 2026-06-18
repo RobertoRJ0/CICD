7658beaa25c4a249ca8ddc6571c852ccc22d5a45
RobertoRamosJo <robys100403rr@gmail.com>
2026-06-18 12:51:36 -0500

    V3: Cambio tipo de dato del promedio a double
    Changed average calculation to use double for precision.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class Main {
  public static void main(String[] args) {
- Scanner sc = new Scanner(System.in);
- int nota1, nota2, nota3, promedio;
- nota1 = leerNota(sc, 1);
- nota2 = leerNota(sc, 2);
- nota3 = leerNota(sc, 3);
- promedio = (nota1 + nota2 + nota3) / 3;
- System.out.println("El promedio es: " + promedio);
+  Scanner sc = new Scanner(System.in);
+  int nota1, nota2, nota3;
+  double promedio;
+  nota1 = leerNota(sc, 1);
+  nota2 = leerNota(sc, 2);
+  nota3 = leerNota(sc, 3);
+  promedio = (nota1 + nota2 + nota3) / 3.0;
+  System.out.println("El promedio es: " + promedio);
  }
  
  public static int leerNota(Scanner sc, int numero) {
@@ -16,7 +17,6 @@ public class Main {
    System.out.print("Ingrese nota " + numero + " (0-20): ");
    nota = sc.nextInt();
   } 
-  while (nota < 0 || nota > 20);
-   return nota;
+   while (nota < 0 || nota > 20);
+    return nota;
  }
-}
